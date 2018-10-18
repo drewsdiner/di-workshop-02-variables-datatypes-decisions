6c6ef8292696c16b4d7692c680130ea6c8898ee2
andrew cochrane <iamandrewcochrane@gmail.com>
2018-10-18 12:12:41 +0100

    updated INDEX AND SKETCH

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var position = 100
+var name = 'Andrew Cochrane'
+var moving = true
+var positionX = 100
+var positionY = 100
+var speedX = 10
+var speedY = 5
+var counter = 0
+
+function setup() {
+  createCanvas(400, 200)
+}
+
+function draw() {
+  background(100)
+  rect(positionX, positionY, 10, 10)
+  text('My name is ' + name, 10, 30)
+  if (moving) {
+    positionX = positionX + speedX;
+    //positionY = positionY + speedY
+    if (positionX >=400 || positionX <0) {
+      speedX = speedX
+    //}if (positionY >=200 || positionY <0){
+    //  speedY = speedY
+    }
+    console.log('The position is ' + position) 
+  }
+
+}
+function mousePressed() {
+  moving = !moving
+}
